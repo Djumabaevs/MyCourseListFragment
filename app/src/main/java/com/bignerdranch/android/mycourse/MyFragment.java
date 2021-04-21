88ff175f87ba92d95991e4905c36d487904ccc94
Bakyt <djumabaevb@gmail.com>
2021-04-21 15:09:18 +0600

    setListAdapter method set

--- app/src/main/java/com/bignerdranch/android/mycourse/MyFragment.java
+++ app/src/main/java/com/bignerdranch/android/mycourse/MyFragment.java
@@ -8,10 +8,26 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.ListFragment;
 
-public class MyFragment extends Fragment {
+import com.bignerdranch.android.mycourse.data.Course;
+import com.bignerdranch.android.mycourse.data.CourseArrayAdapter;
+import com.bignerdranch.android.mycourse.data.CourseData;
+
+import java.util.List;
+
+public class MyFragment extends ListFragment {
+    List<Course> courses = new CourseData().courseList();
 
     public MyFragment() {}
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        CourseArrayAdapter adapter = new CourseArrayAdapter(getActivity(), R.layout.course_listitem, courses);
+        setListAdapter(adapter);
+    }
 
     @Nullable
     @Override
