7315f9245349e622ed65778a82ee55b8dc6fdbbc
Djumabaevs <djumabaevb@gmail.com>
2021-04-22 10:29:58 +0600

    fix unreachable statement

--- app/src/main/java/com/bignerdranch/android/mycourse/CourseDetailFragment.java
+++ app/src/main/java/com/bignerdranch/android/mycourse/CourseDetailFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -21,9 +23,19 @@ public class CourseDetailFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
 
         View view = inflater.inflate(R.layout.course_detail_fragment, container, false);
 
+        if(course != null) {
+            TextView courseName= view.findViewById(R.id.detail_course_name);
+            courseName.setText(course.getCourseName());
+
+            ImageView courseImage = view.findViewById(R.id.detail_course_image);
+            courseImage.setImageResource(course.getImageResourceId(getActivity()));
+
+            TextView courseDescription = view.findViewById(R.id.detail_course_description);
+            courseDescription.setText(course.getCourseName());
+        }
+        return view;
     }
 }
